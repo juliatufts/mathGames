84c2ebf6f8b97bff2f9ad0da85e584f7a559349d
Julia Tufts <juliatufts@gmail.com>
2014-07-17 12:24:54 -0300

    Menu, added particle highlight on start button

---
+++ mathGames/Assets/Scripts/DetectMouse.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectMouse : MonoBehaviour {
+
+	public Ray ray;
+	public RaycastHit hit;
+	public GameObject particleHighlight;
+	private ParticleSystem particles;
+	private bool isEmitting;
+
+	void Start(){
+		particles = particleHighlight.GetComponent<ParticleSystem>();
+	}
+	
+	void Update(){
+		//If the mouse is over START, emit highlight particles
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast(ray, out hit)){
+			if(hit.collider.name == "ParticleHighlight"){
+				particles.enableEmission = true;
+			} else {
+				particles.enableEmission = false;
+			}
+		}
+	}
+}
---
+++ mathGames/Assets/Scripts/DetectedByMouse.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectedByMouse : MonoBehaviour {
+
+	void OnMouseOver()
+	{
+		Debug.Log(gameObject.name);
+	}
+}
