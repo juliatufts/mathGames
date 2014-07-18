75e5285ce8bb6377f55553e0af43cc7b81273c55
Julia Tufts <juliatufts@gmail.com>
2014-07-18 11:45:07 -0300

    Graph Theory, click and drag vertices

---
+++ mathGames/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject mainCamera;
+	private Camera camera;
+	private Transform camTransform;
+
+	public Ray ray;
+	public RaycastHit hit;
+	public bool mouseOnVertex;
+
+	// Use this for initialization
+	void Start () {
+		camera = mainCamera.GetComponent<Camera>();
+		camTransform = mainCamera.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var mousePos = Input.mousePosition;
+		mousePos.z = Mathf.Abs(camTransform.position.z);
+
+		if(Input.GetKey(KeyCode.Mouse0)){
+			//Debug.Log(camera.ScreenToWorldPoint(mousePos));
+		}
+
+		//If the mouse is over a vertex enable click and drag
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast(ray, out hit)){
+			if(hit.collider.name == "Vertex"){
+				mouseOnVertex = true;
+			} else {
+				mouseOnVertex = false;
+			}
+		}
+	}
+}
---
+++ mathGames/Assets/Scripts/VertexController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VertexController : MonoBehaviour {
+	
+	public Ray ray;
+	public RaycastHit hit;
+	public bool canDrag;
+	public bool hasVertex;
+	public float foregroundPosZ;
+
+	// Use this for initialization
+	void Start () {
+		canDrag = false;
+		hasVertex = false;
+	}
+
+	void OnMouseDown(){
+		var mousePos = Input.mousePosition;
+		mousePos.z = Mathf.Abs(Camera.main.transform.position.z) + transform.position.z;
+		foregroundPosZ = Mathf.Abs(Camera.main.transform.position.z) + transform.position.z - 0.5f;
+		
+		//If the mouse is over a vertex, enable click and drag
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Input.GetKey(KeyCode.Mouse0)){
+			if(Physics.Raycast(ray, out hit)){
+				if(hit.collider == collider && !hasVertex){
+					canDrag = true;
+					hasVertex = true;
+				}
+			}
+		} else {
+			canDrag = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var mousePos = Input.mousePosition;
+		mousePos.z = foregroundPosZ;
+
+		if(canDrag){
+			Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+			transform.position = mouseWorldPos;
+		}
+
+	}
+
+	void OnMouseUp() {
+		var mousePos = Input.mousePosition;
+		mousePos.z = foregroundPosZ + 0.5f;
+
+		if(canDrag){
+			Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+			transform.position = mouseWorldPos;
+		}
+
+		canDrag = false;
+		hasVertex = false;
+	}
+}
