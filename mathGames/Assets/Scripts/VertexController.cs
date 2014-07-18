fe98b4fc228fc2494e8f6c439a827e77f32f32f4
Julia Tufts <juliatufts@gmail.com>
2014-07-18 16:28:25 -0300

    Graph Theory, can create vertex and move around about creation

--- mathGames/Assets/Scripts/VertexController.cs
+++ mathGames/Assets/Scripts/VertexController.cs
@@ -8,6 +8,8 @@ public class VertexController : MonoBehaviour {
 	public bool canDrag;
 	public bool hasVertex;
 	public float foregroundPosZ;
+	private float click_timer;
+	public float hold_delay;
 
 	// Use this for initialization
 	void Start () {
@@ -16,28 +18,28 @@ public class VertexController : MonoBehaviour {
 	}
 
 	void OnMouseDown(){
-		var mousePos = Input.mousePosition;
-		mousePos.z = Mathf.Abs(Camera.main.transform.position.z) + transform.position.z;
+		click_timer = Time.time;
 		foregroundPosZ = Mathf.Abs(Camera.main.transform.position.z) + transform.position.z - 0.5f;
-		
-		//If the mouse is over a vertex, enable click and drag
-		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		if(Input.GetKey(KeyCode.Mouse0)){
-			if(Physics.Raycast(ray, out hit)){
-				if(hit.collider == collider && !hasVertex){
-					canDrag = true;
-					hasVertex = true;
-				}
-			}
-		} else {
-			canDrag = false;
-		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	//While mouse click is down, update vertex position
+	void OnMouseDrag(){
+
+		if((Time.time - click_timer) > hold_delay){
+			Debug.Log("clickhold");
+		}
+
+		//Get mouse position
 		var mousePos = Input.mousePosition;
 		mousePos.z = foregroundPosZ;
+		//If the mouse click hold is over a vertex, enable click and drag
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast(ray, out hit)){
+			if(hit.collider == collider && !hasVertex){
+				canDrag = true;
+				hasVertex = true;
+			}
+		}
 
 		if(canDrag){
 			Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
@@ -46,6 +48,7 @@ public class VertexController : MonoBehaviour {
 
 	}
 
+	//When mouse click release, set position
 	void OnMouseUp() {
 		var mousePos = Input.mousePosition;
 		mousePos.z = foregroundPosZ + 0.5f;
@@ -53,9 +56,8 @@ public class VertexController : MonoBehaviour {
 		if(canDrag){
 			Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
 			transform.position = mouseWorldPos;
+			canDrag = false;
+			hasVertex = false;
 		}
-
-		canDrag = false;
-		hasVertex = false;
 	}
 }
---
+++ mathGames/Assets/Scripts/createVertex.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class createVertex : MonoBehaviour {
+
+	public Ray ray;
+	public RaycastHit hit;
+	public GameObject vertex;
+	private List<GameObject> vertices;
+	private bool canDrag = false;
+
+	public bool one_click = false;
+	public float foregroundPosZ;
+	public bool timer_running;
+	private float timer_for_double_click;
+	public float delay;			//How long in seconds to allow for a double click
+
+	void Start(){
+		vertices = new List<GameObject>();
+	}
+
+	void OnMouseDown(){
+		//Get Mouse position, z position forward
+		var mousePos = Input.mousePosition;
+		foregroundPosZ = Mathf.Abs(Camera.main.transform.position.z) + vertex.transform.position.z - 0.5f;
+		mousePos.z = foregroundPosZ;
+		Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+		//First click, no previous clicks
+		if(!one_click){
+			one_click = true;
+			timer_for_double_click = Time.time; //Save the current time
+			//Single click action
+		} else {
+			one_click = false; //Found a double click, now reset
+			//If cursor is on graph paper, create a new vertex
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(ray, out hit)){
+				if(hit.collider == collider){
+					var newVertex = GameObject.Instantiate(vertex, mouseWorldPos, vertex.transform.rotation) as GameObject;
+					vertices.Add(newVertex);
+					newVertex.transform.parent = transform;
+				}
+				canDrag = true;
+			}
+			
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(one_click){
+			//If the time now is delay seconds more than when the first click started, reset
+			if((Time.time - timer_for_double_click) > delay){
+				one_click = false;
+			}
+		}
+		
+	}
+	
+	//While mouse click is down, update new vertex position
+	void OnMouseDrag () {
+		var mousePos = Input.mousePosition;
+		mousePos.z = foregroundPosZ;
+		
+		if(canDrag){
+			Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+			vertices[vertices.Count - 1].transform.position = mouseWorldPos;
+		}
+		
+	}
+	
+	//When mouse click release, set position
+	void OnMouseUp() {
+		if(canDrag){
+			//Set mouse position, adjust z value
+			var mousePos = Input.mousePosition;
+			mousePos.z = foregroundPosZ + 0.5f;
+
+			Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+			vertices[vertices.Count - 1].transform.position = mouseWorldPos;
+		}
+		
+		canDrag = false;
+	}
+
+}
